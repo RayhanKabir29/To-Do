90ed8261b544d7353115179fea8dc91e1be7cb38
RayhanKabir29 <rayhan.kabir29@gmail.com>
2025-03-02 14:59:46 +0600

    create store

---
+++ src/redux/rootReducer.js
@@ -0,0 +1,10 @@
+import filterReducer from "./filter/reducer"
+import todoReducer from "./todos/reducer"
+import { combineReducers } from "redux"
+
+const rootReducer = combineReducers({
+    todos: todoReducer,
+    filter: filterReducer
+})
+
+export default rootReducer;
